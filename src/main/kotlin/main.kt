c06958d2d07c0b0179ab47557c5ee0fc2df24ddd
wolfgang <wolfgang.loeffler@gmail.com>
2024-03-10 16:01:45 +0100

    fix map webview leaflet -> openlayer4

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -58,32 +58,55 @@ enum class QuickOperation { COPY, MOVE, GO }
 enum class Zoom { IN, OUT, FIT}
 
 class InfoView(private val mv: MainView): View("Information") {
-    private fun leafletHTML(geo: GeoLocation) = """
-                            <!DOCTYPE html>
-                            <html>
-                            <head>
-                            	<meta charset="utf-8" />
-                            	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-                                <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
-                                <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossorigin=""></script>
-                            </head>
-                            <body>
-                            <div id="mapid" style="width: 350px; height: 250px;"></div>
-                            <script>
-                            	var mymap = L.map('mapid').setView([${geo.latitude}, ${geo.longitude}], 13);
-                            	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-                            		maxZoom: 18,
-                            		attribution: '<a href="https://www.openstreetmap.org/">OpenStreetMap</a> , ' +
-                            			'<a href="https://www.mapbox.com/">Mapbox</a>',
-                            		id: 'mapbox/streets-v11',
-                            		tileSize: 512,
-                            		zoomOffset: -1
-                            	}).addTo(mymap);
-                            	L.marker([${geo.latitude}, ${geo.longitude}]).addTo(mymap);
-                            </script>
-                            </body>
-                            </html>
-                            """.trimIndent()
+    //	https://leafletjs.com/examples/quick-start/
+    // TODO leaflet doesn't work with jfx 21: https://stackoverflow.com/questions/77764181/webview-in-javafx-not-showing-leaflet-map-correct
+    // openlayers 9 also doesn't, but 4 !!
+    private fun mapHTML(geo: GeoLocation) = """
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>Simple Map</title>
+            <link rel="stylesheet" href="https://openlayers.org/en/v4.6.5/css/ol.css" type="text/css">
+            <script src="https://cdn.jsdelivr.net/npm/ol@v9.0.0/dist/ol.js"></script>
+            <style>
+              html, body {
+              height: 100%;
+              }
+              #map {width:100%; height:100%;}
+            </style>
+          </head>
+          <body>
+            <script src="https://openlayers.org/en/v4.6.5/build/ol.js" type="text/javascript"></script>
+            <div id="map" class="map"></div>
+            <script>
+                var baseMapLayer = new ol.layer.Tile({
+                  source: new ol.source.OSM()
+                });
+                var map = new ol.Map({
+                  target: 'map',
+                  layers: [ baseMapLayer],
+                  view: new ol.View({
+                          center: ol.proj.fromLonLat([${geo.longitude}, ${geo.latitude}]), 
+                          zoom: 13
+                        })
+                });
+                //Adding a marker on the map
+                var marker = new ol.Feature({
+                  geometry: new ol.geom.Point(
+                    ol.proj.fromLonLat([${geo.longitude}, ${geo.latitude}])
+                  ),
+                });
+                var vectorSource = new ol.source.Vector({
+                  features: [marker]
+                });
+                var markerVectorLayer = new ol.layer.Vector({
+                  source: vectorSource,
+                });
+                map.addLayer(markerVectorLayer);
+            </script>
+          </body>
+          </html>
+    """.trimIndent()
 
     override val root = scrollpane {
         form {
@@ -114,7 +137,7 @@ class InfoView(private val mv: MainView): View("Information") {
                             prefWidth = 350.0
                             prefHeight = 270.0
                             runLater {
-                                engine.loadContent(leafletHTML(geo))
+                                engine.loadContent(mapHTML(geo))
                             }
                         }
                     }
